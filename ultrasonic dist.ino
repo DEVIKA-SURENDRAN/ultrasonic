2100d06d3d98b56ba1a633443d8ebd488acc444d
devika surendran <devikasurendran309@gmail.com>
2024-06-26 12:06:09 +0530

    Add files via upload

---
+++ ultrasonic dist.ino
@@ -0,0 +1,26 @@
+int echopin=13;
+int trigpin=12;
+long duration;
+long distance;
+void setup() {
+Serial.begin(9600);
+pinMode(trigpin,OUTPUT);
+pinMode(echopin,INPUT);
+  
+}
+
+void loop() {
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+
+  duration = pulseIn(echopin,HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance:");
+  Serial.print(distance);
+  Serial.println(" cm ");
+  delay(500);
+}
+
